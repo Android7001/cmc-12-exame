d2895171e84030d3737f16e1244e26cc93976da6
davil2020 <davilessa2002@gmail.com>
2024-07-09 15:55:10 -0300

    ixqua2

--- foot.py
+++ foot.py
@@ -1,25 +1,46 @@
-#interpolação f(psi)em x, y, psi
-
-#interpolação g(psi) em z
-
 import math
+import numpy as np
 
-t_b = t_e = t = T = 1
-
-psi = t/T
-psi_b = t_b/T
-psi_e = t_e/T
-
-if psi <= psi_b:
-    f = 0
-elif psi > psi_b and psi <= psi_e:
-    f = 0.5*(1 - math.cos(math.pi * (psi - psi_b) / (psi_e - psi_b)))
-else:
-    f = 1
+t_inicio = 0
+t_fim = 1
+passo = 0.01
+T = 1 
 
 
-if psi <= psi_b or psi > psi_e:
-    g = 0
-else:   
-    g = 0.5 * (1 - math.cos(2 * math.pi * (psi - psi_b) / (psi_e - psi_b)))
+t_b = 0.4
+t_e = 0.6
+psi_b = t_b / T
+psi_e = t_e / T
 
+vetor_tempo = np.arange(t_inicio, t_fim, passo)
+
+
+def get_f(psi):
+    if psi <= psi_b:
+        return 0
+    elif psi > psi_b and psi <= psi_e:
+        return 0.5 * (1 - math.cos(math.pi * (psi - psi_b) / (psi_e - psi_b)))
+    else:
+        return 1
+
+def get_g(psi):
+    if psi <= psi_b or psi > psi_e:
+        return 0
+    else:
+        return 0.5 * (1 - math.cos(2 * math.pi * (psi - psi_b) / (psi_e - psi_b)))
+
+
+F = []
+G = []
+
+
+for tempo in vetor_tempo:
+    psi = tempo % T  
+    f = get_f(psi)
+    g = get_g(psi)
+    F.append(f)
+    G.append(g)
+
+
+print("F:", F)
+print("G:", G)
